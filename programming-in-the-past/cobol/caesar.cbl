598b9ce103036c57d4da9f333e7dd984d04ab364
Josh Seligman <joshua.seligman1@marist.edu>
2023-01-20 09:19:28 -0500

    Fixed the encrypt function in cobol

--- programming-in-the-past/cobol/caesar.cbl
+++ programming-in-the-past/cobol/caesar.cbl
@@ -4,15 +4,17 @@
 000000   Working-storage section.
 000000     1 curChar pic 999.
 000000     1 idx pic 99 value 1.
-000000     1 diff pic S99.
+000000     1 diff pic S9(2).
+000000     1 newShift pic S9(2).
 000003   Linkage section.
 000004     1 inStr pic x(32).
-000000     1 shiftAmt pic 99.
+000000     1 shiftAmt pic S999.
 000005     1 res pic x(32).
 000006 Procedure division
 000007   using by reference inStr shiftAmt
 000008   returning res.
 000009   Move inStr to res
+000000   compute newShift = function mod(shiftAmt, 26)
 000000   display inStr " - " shiftAmt
 000000   perform encrypt-work until idx > 32
 000011   goback.
@@ -22,10 +24,15 @@
 000000       compute curChar = curChar - 32
 000000     end-if
 000000     if curChar >= 66 and curChar <= 91 then
-000000       compute curChar = curChar + shiftAmt
+000000       compute curChar = curChar + newShift
 000000       compute diff = curChar - 91
 000000       if diff > 0 then
 000000         compute curChar = 66 + diff - 1
+000000       else
+               compute diff = 66 - curChar
+               if (diff > 0) then
+                 compute curChar = 91 - diff + 1
+               end-if
 000000       end-if
 000000       move function char(curChar) to res(idx:1)
 000000     end-if
@@ -40,9 +47,10 @@
 000106 Data division.
 000107   Working-storage section.
 000108     1 inStr pic x(32) value "This is a test string from Alan".
+000000     1 shift pic S999 value 8.
 000108     1 result pic x(32) usage display.
 000109 Procedure division.
-000110   Move encrypt(inStr 08) to result
+000110   Move encrypt(inStr shift) to result
 000111   Display result
 000112   Goback.
 000113 End program caesar.
