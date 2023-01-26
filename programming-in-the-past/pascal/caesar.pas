7878350ecaac3438ce35e8b1405f0fc14bdc1dc7
Josh Seligman <joshua.seligman1@marist.edu>
2023-01-26 18:23:52 -0500

    Wrote encrypt in pascal

--- programming-in-the-past/pascal/caesar.pas
+++ programming-in-the-past/pascal/caesar.pas
@@ -2,16 +2,39 @@ program Caesar;
 function Encrypt(inStr : string; shiftAmount : integer) : string;
 var
     curChar: Char;
+    charValue: integer;
+    diff: integer;
+    idx: integer;
 begin
     shiftAmount := shiftAmount mod 26;
     Encrypt := inStr;
+    idx := 1;
     for curChar in Encrypt do
-        writeln(curChar);
+    begin
+        charValue := ord(curChar);
+        if ((charValue >= 97) and (charValue <= 122)) then
+            charValue := charValue - 32;
+        if ((charValue >= 65) and (charValue <= 90)) then
+        begin
+            charValue := charValue + shiftAmount;
+            diff := charValue - 90;
+            if diff > 0 then
+                charValue := 65 + diff - 1
+            else
+            begin
+                diff := 65 - charValue;
+                if diff > 0 then
+                    charValue := 90 - diff + 1;
+            end;
+            Encrypt[idx] := chr(charValue);
+        end;
+        idx := idx + 1;
+    end;
 end;
 
 var
     originalString : string;
 begin
-    originalString := 'hello world';
-    Encrypt(originalString, 28);
+    originalString := 'This is a test string from Alan';
+    writeln(Encrypt(originalString, -18));
 end.
