336fe2159f133435afa989181acbfd8d6d10c0b3
Josh Seligman <joshua.seligman1@marist.edu>
2023-01-26 18:25:53 -0500

    Wrote decrypt in pascal

--- programming-in-the-past/pascal/caesar.pas
+++ programming-in-the-past/pascal/caesar.pas
@@ -32,9 +32,47 @@ begin
     end;
 end;
 
+function Decrypt(inStr : string; shiftAmount : integer) : string;
+var
+    curChar: Char;
+    charValue: integer;
+    diff: integer;
+    idx: integer;
+begin
+    shiftAmount := shiftAmount mod 26;
+    Decrypt := inStr;
+    idx := 1;
+    for curChar in Decrypt do
+    begin
+        charValue := ord(curChar);
+        if ((charValue >= 97) and (charValue <= 122)) then
+            charValue := charValue - 32;
+        if ((charValue >= 65) and (charValue <= 90)) then
+        begin
+            charValue := charValue - shiftAmount;
+            diff := charValue - 90;
+            if diff > 0 then
+                charValue := 65 + diff - 1
+            else
+            begin
+                diff := 65 - charValue;
+                if diff > 0 then
+                    charValue := 90 - diff + 1;
+            end;
+            Decrypt[idx] := chr(charValue);
+        end;
+        idx := idx + 1;
+    end;
+end;
+
 var
     originalString : string;
+    encryptOut : string;
+    decryptOut : string;
 begin
     originalString := 'This is a test string from Alan';
-    writeln(Encrypt(originalString, -18));
+    encryptOut := Encrypt(originalString, 8);
+    decryptOut := Decrypt(encryptOut, 8);
+    writeln(encryptOut);
+    writeln(decryptOut);
 end.
